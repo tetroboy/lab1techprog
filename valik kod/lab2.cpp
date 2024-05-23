e676af6531a74a25d06e7c653c7e8f3c69f71c86
V1zav <valikzav2002@gmail.com>
2024-05-23 09:26:52 +0300

    changed code, after refactoring

--- valik kod/lab2.cpp
+++ valik kod/lab2.cpp
@@ -5,14 +5,12 @@
 #include <stdexcept>
 
 template <typename T>
-class LinkedList
-{
+class LinkedList {
 private:
-    struct Node
-    {
+    struct Node {
         T data;
         std::unique_ptr<Node> next;
-        Node(const T &data) : data(data), next(nullptr) {}
+        Node(const T& data) : data(data), next(nullptr) {}
     };
 
     std::unique_ptr<Node> head;
@@ -21,25 +19,19 @@ private:
 public:
     LinkedList() : head(nullptr), size_(0) {}
 
-    LinkedList(std::initializer_list<T> list) : LinkedList()
-    {
-        auto it = list.end();
-        while (it != list.begin())
-        {
+    LinkedList(std::initializer_list<T> list) : LinkedList() {
+        for (auto it = list.end(); it != list.begin(); ) {
             --it;
             push_front(*it);
         }
     }
 
-    LinkedList(LinkedList &&other) noexcept : head(std::move(other.head)), size_(other.size_)
-    {
+    LinkedList(LinkedList&& other) noexcept : head(std::move(other.head)), size_(other.size_) {
         other.size_ = 0;
     }
 
-    LinkedList &operator=(LinkedList &&other) noexcept
-    {
-        if (this != &other)
-        {
+    LinkedList& operator=(LinkedList&& other) noexcept {
+        if (this != &other) {
             head = std::move(other.head);
             size_ = other.size_;
             other.size_ = 0;
@@ -49,58 +41,50 @@ public:
 
     ~LinkedList() = default;
 
-    void push_front(const T &value)
-    {
-        auto newNode = std::make_unique<Node>(value);
-        newNode->next = std::move(head);
-        head = std::move(newNode);
+    void push_front(const T& value) {
+        auto new_node = std::make_unique<Node>(value);
+        new_node->next = std::move(head);
+        head = std::move(new_node);
         ++size_;
     }
 
-    void push_front(T &&value)
-    {
-        auto newNode = std::make_unique<Node>(std::move(value));
-        newNode->next = std::move(head);
-        head = std::move(newNode);
+    void push_front(T&& value) {
+        auto new_node = std::make_unique<Node>(std::move(value));
+        new_node->next = std::move(head);
+        head = std::move(new_node);
         ++size_;
     }
 
-    size_t size() const
-    {
+    size_t get_size() const {
         return size_;
     }
 
-    void clear()
-    {
-        while (head)
-        {
+    void clear_list() {
+        while (head) {
             head = std::move(head->next);
         }
         size_ = 0;
     }
 
-    LinkedList<T> split_when(std::function<bool(const T &)> condition)
-    {
+    LinkedList<T> split_when(std::function<bool(const T&)> condition) {
         LinkedList<T> result;
-        if (!head)
-            return result;
+        if (!head) return result;
 
         size_t tail_size = 0;
 
-        auto *current = head.get();
-        if (condition(current->data))
-        {
+        auto* current = head.get();
+        if (condition(current->data)) {
             result = std::move(*this);
-            clear();
+            clear_list();
             return result;
         }
-        while (current->next && !condition(current->next->data))
-        {
+
+        while (current->next && !condition(current->next->data)) {
             current = current->next.get();
             ++tail_size;
         }
-        if (!current->next)
-            return result;
+
+        if (!current->next) return result;
 
         result.head = std::move(current->next);
         result.size_ = size_ - tail_size - 1;
@@ -111,11 +95,9 @@ public:
         return result;
     }
 
-    friend std::ostream &operator<<(std::ostream &os, const LinkedList &list)
-    {
-        auto *current = list.head.get();
-        while (current)
-        {
+    friend std::ostream& operator<<(std::ostream& os, const LinkedList& list) {
+        auto* current = list.head.get();
+        while (current) {
             os << current->data << " ";
             current = current->next.get();
         }
@@ -123,37 +105,30 @@ public:
     }
 };
 
-int main()
-{
-    try
-    {
+int main() {
+    try {
         LinkedList<int> original = {1, 25, 42, 3, 5, 9, 8, 13, 10, 9};
 
         LinkedList<int> empty_list;
-        if (empty_list.size() != 0)
-        {
+        if (empty_list.get_size() != 0) {
             throw std::runtime_error("Default constructor failed!");
         }
 
         std::cout << "Original list: " << original << std::endl;
 
-        auto tail = original.split_when([](const int &value)
-                                        { return value == 9; });
+        auto tail = original.split_when([](const int& value) { return value == 9; });
 
-        std::cout << "Original list size after split: " << original.size() << std::endl;
-        std::cout << "Tail list size: " << tail.size() << std::endl;
+        std::cout << "Original list size after split: " << original.get_size() << std::endl;
+        std::cout << "Tail list size: " << tail.get_size() << std::endl;
 
         std::cout << "Original list after split: " << original << std::endl;
         std::cout << "Tail list: " << tail << std::endl;
 
-        original.clear();
-        if (original.size() != 0)
-        {
+        original.clear_list();
+        if (original.get_size() != 0) {
             throw std::runtime_error("Clear method failed!");
         }
-    }
-    catch (const std::exception &e)
-    {
+    } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
 
